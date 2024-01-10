48a90ff6cfeac5164bdb86b8e28ea3f60146a563
Ray Patton <geraldr.patton@gmail.com>
2024-01-09 22:03:11 -0800

    Code cleanup of Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,15 +16,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
 
-        //if player position on the y is greater than 0
-        //y position = 0
-        //else if position on the y is less than -3.9f
-        //y position = -3.9f
 
         if (transform.position.y >= 0)
         {
@@ -35,11 +36,6 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, -3.9f, 0);
         }
 
-        //if player on the x > 11.3f
-        //x pos = -11.3f
-        //else if the player on the x axis is less than -11.3f
-        //x pos = 11.3f
-
         if (transform.position.x > 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
@@ -48,6 +44,5 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
-
     }
 }
