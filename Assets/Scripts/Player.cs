e9f6f1d2562547b6c5f3049e66721896a7f200e5
Ray Patton <geraldr.patton@gmail.com>
2024-01-09 20:38:44 -0800

    Set limit on y axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //public or private reference
-    //data type (int, float, bool, string)
-    //every variable has a name
-    //optional value assigned
-
     [SerializeField]
     private float _speed = 4.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //take the current position = new position (0, 0, 0)
         transform.position = new Vector3 (0, 0, 0);
     }
 
@@ -24,10 +18,24 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-                //new Vector 3(1, 0, 0) 5 * realtime
-                //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-                //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
 
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+
+        //if player position on the y is greater than 0
+        //y position = 0
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+
+
+
+        //if player on the x > 13
+        //x pos = -13
+        //else if the player on the x axis is less than -13
+        //x pos = 13
+
+
     }
 }
