f992c6eb9c1130cca15896d702fe93f3429e6441
Ray Patton <geraldr.patton@gmail.com>
2024-01-17 18:22:24 -0800

    Create new Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move down at 4 meters per second
+
+        //if bottom of screen
+        //respawn at top a new random x position
+    }
+}
