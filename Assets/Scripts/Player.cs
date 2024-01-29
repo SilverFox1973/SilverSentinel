8aa7b4535b402029290e5747669d475ac020c7f6
Ray Patton <geraldr.patton@gmail.com>
2024-01-28 20:26:22 -0800

    Lives variable added to Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _fireRate = 0.25f;
     private float _nextFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
 
     // Start is called before the first frame update
     void Start()
