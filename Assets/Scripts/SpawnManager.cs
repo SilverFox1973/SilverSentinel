1b6d6cd23f1b6761b58bb436b29f516e26f5ec73
Ray Patton <geraldr.patton@gmail.com>
2024-02-10 19:13:24 -0800

    Spawning stops on Player Death added

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,8 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyContainer;
 
+    private bool _stopSpawning = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,16 +29,18 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnRoutine()
     {
-        while (true) 
+        while (_stopSpawning == false) 
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-9f, 9f), 7, 0);
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
         }
-        //while loop (infinite loop)
-            //Instatiate enemy prefab
-            //yield wait for 5 seconds
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
     }
 
 }
