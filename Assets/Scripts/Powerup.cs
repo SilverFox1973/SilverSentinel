4013c5ffb8fdbce1dc764df4f6eb1005720fa654
Ray Patton <geraldr.patton@gmail.com>
2024-03-02 11:25:44 -0800

    Triple Shot Cool down added

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -35,6 +35,13 @@ public class Powerup : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            //communicate with the Player script
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+            {
+                player.TripleShotActive();
+            }
+
             Destroy(this.gameObject);
         }
     }
