25160c79e1ec62dcc8c3c50e60f2a429a74c9d16
Ray Patton <geraldr.patton@gmail.com>
2024-03-11 20:28:36 -0700

    Triple Shot PowerUp Animation added

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -14,6 +14,13 @@ public class Laser : MonoBehaviour
 
         if (transform.position.y >= 8)
         {
+            //check if this object has a parent. 
+            //destroy the parent too!
+            if (transform.parent != null)
+            { 
+                Destroy(transform.parent.gameObject);
+            }
+
             Destroy(this.gameObject);
         }
 
