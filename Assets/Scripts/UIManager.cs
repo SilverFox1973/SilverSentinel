77c1e0e72b78f7046c305658a39d60a5571f6a9c
Ray Patton <geraldr.patton@gmail.com>
2024-04-05 16:44:08 -0700

    Added Lives counter to UI.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,14 +10,25 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TMP_Text _scoreText;
 
+    [SerializeField]
+    private Sprite[] _liveSprites;
+    [SerializeField]
+    private Image _LivesImg;
     // Start is called before the first frame update
     void Start()
-    {
+    {    
         _scoreText.text = "Score: " + 00;
     }
 
     public void UpdateScore(int playerScore)
     {
         _scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        //display image sprite
+        //give it a new one based on the currentLives index
+        _LivesImg.sprite = _liveSprites[currentLives];
     }
 }
