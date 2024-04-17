c3363d510cb1851cccabf0d481d531152acfc00e
Ray Patton <geraldr.patton@gmail.com>
2024-04-16 20:27:31 -0700

    Added Game start after Asteroid is destroyed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -12,17 +12,18 @@ public class SpawnManager : MonoBehaviour
     private GameObject[] powerups;
 
     private bool _stopSpawning = false;
+   
 
-    // Start is called before the first frame update
-    void Start()
+    public void StartSpawning()
     {
         StartCoroutine(SpawnEnemyRoutine());
         StartCoroutine(SpawnPowerupRoutine());
     }
 
-
     IEnumerator SpawnEnemyRoutine()
     {
+        yield return new WaitForSeconds(3.0f);
+
         while (_stopSpawning == false) 
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-9f, 9f), 7, 0);
@@ -33,7 +34,9 @@ public class SpawnManager : MonoBehaviour
     }
 
     IEnumerator SpawnPowerupRoutine()
-    { 
+    {
+        yield return new WaitForSeconds(3.0f);
+
         while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-9f, 9f), 7, 0);
