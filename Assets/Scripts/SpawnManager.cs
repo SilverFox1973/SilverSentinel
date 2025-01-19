fc13da03cb549c1d84455b967b08b7b6533268d3
Ray Patton <geraldr.patton@gmail.com>
2025-01-19 12:38:16 -0800

    Change Random Range on Spawn Manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -41,7 +41,7 @@ public class SpawnManager : MonoBehaviour
 
         while (_stopSpawning == false) 
         {
-            Vector3 posToSpawn = new Vector3(Random.Range(-9f, 9f), 7, 0);
+            Vector3 posToSpawn = new Vector3(Random.Range(-12f, 12f), 7, 0);
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
